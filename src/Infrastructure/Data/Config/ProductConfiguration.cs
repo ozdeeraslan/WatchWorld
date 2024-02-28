446d0264f89b5fe5c9b6348044ab1e2720c3aa6b
ozdeeraslan <ozdenureraslan@gmail.com>
2024-02-28 10:32:49 +0100

    interfaces-data-config

---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(200);
+            builder.Property(p => p.Price).HasPrecision(18, 2);
+        }
+    }
+}
