ab964e2282e0128877a33554679b112207ed35c5
ozdeeraslan <ozdenureraslan@gmail.com>
2024-03-04 10:30:02 +0100

    home view model

---
+++ src/Web/Models/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Models
+{
+    public class HomeViewModel
+    {
+        public List<CatalogItemViewModel> CatalogItems { get; set; } = new List<CatalogItemViewModel>();
+
+        public List<SelectListItem>? Brands { get; set; } = new List<SelectListItem>();
+
+        public List<SelectListItem>? Categories { get; set; } = new List<SelectListItem>();
+
+        public int? CategoryId { get; set; }
+
+        public int? BrandId { get; set; }
+    }
+}
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -3,6 +3,8 @@ using Infrastructure.Data;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Web.Interfaces;
+using Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ builder.Services.AddDbContext<WatchWorldContext>(options => options.UseNpgsql(bu
 builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("AppIdentityDbContext")));
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>)); // DI configuration
+
+builder.Services.AddScoped<IHomeViewModelService, HomeViewModelService>();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
@@ -37,6 +41,8 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRequestLocalization("en-US");
 
 app.UseRouting();
 
